57bda1bfa24ea7df602f021849276c0008eede72
achikhaoui05 <achikhaoui05@iesalbarregas.es>
2025-10-10 11:56:31 +0200

    formularioJSP

---
+++ modules/enemies.js
@@ -0,0 +1,36 @@
+export class Enemigo {
+    type;
+    name;
+    levelAtaque;
+    hp; 
+
+    constructor(type,name,levelAtaque,hp){
+        this.type='Enemigo';
+        this.name=name;
+        this.levelAtaque=levelAtaque;
+        this.hp=hp;
+    }
+    action(){
+        return `${this.type} ${this.name} ${this.levelAtaque} ${this.hp}`;
+    }
+    }
+
+    export class JefeFinal extends Enemigo {
+    specialSkill;
+    multiplierDamage;
+    constructor(name,levelAtaque,hp,pecialSkill,multiplierDamage=2 ){
+        super(name,levelAtaque,hp)
+        this.type= 'Jefe';
+        this.pecialSkill=pecialSkill;
+        this.multiplierDamage=this.multiplierDamage;
+        
+    }
+
+    action(){
+        return `Soy  ${this.name}. El enemigo tiene una habilidad especial :  ${this.pecialSkill} `;
+    }
+    ataquePotenciado(){
+        return this.levelAtaque * this.multiplierDamage;
+    }
+
+}
